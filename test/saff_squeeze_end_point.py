77a6c9811d56531acade49baf088765fd749dc16
jmasonlee <jmasonlee@gmail.com>
2022-04-18 16:26:22 -0600

    t Reduce remaining dfs

--- test/saff_squeeze_end_point.py
+++ test/saff_squeeze_end_point.py
@@ -10,28 +10,48 @@ from pandemic_recovery_batch import transform
 
 def test_will_count_reviews_without_matching_checkins(spark: SparkSession) -> None:
     run_date = datetime(2022, 4, 14).strftime('%Y-%m-%d')
-    reviews_df = spark.createDataFrame(
-        [(run_date, "fake_business_id")], StructType([StructField("date", StringType()), StructField("business_id", StringType())]))
-    checkin_df = spark.createDataFrame(
-        [], StructType([StructField("date", DateType()), StructField("business_id", StringType())]))
-    tips_df = spark.createDataFrame(
-        [], StructType([StructField("date", DateType()), StructField("business_id", StringType())]))
-    business_df = spark.createDataFrame(
-        [("fake_business_id", "fake_business")], StructType([StructField("business_id", StringType()),StructField("name", StringType())]))
-    mobile_reviews_df = spark.createDataFrame(
-        [(run_date, "fake_business_id")], StructType([StructField("date", StringType()), StructField("business_id", StringType())]))
+
+    enrichment_columns = StructType([
+         StructField("date", StringType()),
+         StructField("business_id", StringType())
+    ])
+    review_data = [(run_date, "fake_business_id")]
+
+    business_df  = setup_business_df(spark)
+
+    checkin_df   = setup_unused_df(enrichment_columns, spark)
+    tips_df      = setup_unused_df(enrichment_columns, spark)
+
+    reviews_df   = setup_review_df(enrichment_columns, review_data, spark)
+    m_reviews_df = setup_review_df(enrichment_columns, review_data, spark)
+
 
     actual_df = transform(
         business_df,
         checkin_df,
         reviews_df,
         tips_df,
-        mobile_reviews_df,
+        m_reviews_df,
         run_date
     )
 
     actual_json = data_frame_to_json(actual_df)
     assert actual_json[0]["num_reviews"] == 2
+
+
+def setup_review_df(enrichment_columns, review_data, spark):
+    return spark.createDataFrame(data=review_data, schema=enrichment_columns)
+
+def setup_unused_df(enrichment_columns, spark):
+    return spark.createDataFrame(data=[], schema=enrichment_columns)
+
+def setup_business_df(spark):
+    base_business_info = [("fake_business_id", "fake_business")]
+    important_business_columns = StructType([
+        StructField("business_id", StringType()),
+        StructField("name", StringType())
+    ])
+    return spark.createDataFrame(data=base_business_info, schema=important_business_columns)
 
 
 def create_df_from_json(json_file, spark):
