5dd40020df1157441fac514927a3207aca8941a9
jmasonlee <jmasonlee@gmail.com>
2022-05-11 15:33:05 -0600

    Use user_id

--- pandemic_recovery_batch.py
+++ pandemic_recovery_batch.py
@@ -14,8 +14,13 @@ def transform(business_df: DataFrame,
               run_date: datetime = datetime.today()):
 
     checkin_df = checkin_df.withColumn("checkins_list", F.split(checkin_df.date, ","))
-    checkin_df = checkin_df.select(F.col("business_id"), F.explode(F.col("checkins_list")).alias("date"))
-    checkin_df = checkin_df.withColumn('date', F.trim(checkin_df.date))
+    checkin_df = checkin_df.withColumn("date", F.explode(checkin_df.checkins_list))
+    checkin_df = checkin_df.withColumn("date", F.trim(checkin_df.date))
+    checkin_df = checkin_df.select(
+        F.col("business_id"),
+        F.col("user_id"),
+        F.col("date")
+    )
 
 
     reviews_df = count_reviews(checkin_df, mobile_reviews_df, browser_reviews_df, run_date)
@@ -58,8 +63,7 @@ def count_checkins(checkin_df, run_date):
 
 
 def count_reviews(checkin_df, mobile_reviews_df, browser_reviews_df, run_date):
-    mobile_reviews_df = mobile_reviews_df.join(checkin_df, on=['business_id','date'], how="left_anti").select(*mobile_reviews_df.columns)
-    mobile_reviews_df.show()
+    mobile_reviews_df = mobile_reviews_df.join(checkin_df, on=['business_id','date', 'user_id'], how="left_anti").select(*mobile_reviews_df.columns)
     reviews_df = mobile_reviews_df.union(browser_reviews_df)
     reviews_df = reviews_df.filter(reviews_df.date == run_date.date())
     reviews_df = reviews_df.groupby("business_id").count()
