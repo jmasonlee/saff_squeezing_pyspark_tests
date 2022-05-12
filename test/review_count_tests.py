cfa61bf4b09745195bd246fab5181876756d1d83
jmasonlee <jmasonlee@gmail.com>
2022-05-12 17:21:52 -0600

    delete unimportant code

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -18,7 +18,6 @@ def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
     checkin_df = count_checkins(checkin_df, date)
 
     pandemic_recovery_df = business_df.join(checkin_df, on="business_id", how='left').fillna(0)
-    pandemic_recovery_df = pandemic_recovery_df.join(reviews_df, on="business_id", how='left').fillna(0)
     inglewood_pizza = data_frame_to_json(reviews_df)[6]
     assert inglewood_pizza["business_id"] == "mpf3x-BjTdTEA3yCZrAYPw"
     assert inglewood_pizza["num_reviews"] == 1
