a4ecbb830bf487310865c73282140912af54bdad
jmasonlee <jmasonlee@gmail.com>
2022-05-12 17:57:44 -0600

    Delete unimportant code

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -12,7 +12,6 @@ def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
     m_reviews_df = create_df_from_json("fixtures/mobile_reviews.json", spark)
     date = datetime(2022, 4, 14)
 
-    checkin_df = create_df_from_json("fixtures/checkin.json", spark)
     checkin_df = create_df_from_json("fixtures/checkins_exploded.json", spark)
     reviews_df = count_reviews(checkin_df, m_reviews_df, b_reviews_df, date)  # <- This is what we care about
 
