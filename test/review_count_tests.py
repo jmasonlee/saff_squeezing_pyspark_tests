0005615ada5752aa890d5466f70e88750323af04
jmasonlee <jmasonlee@gmail.com>
2022-05-12 17:08:49 -0600

    delete unimportant code

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -24,10 +24,7 @@ def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
     inglewood_pizza = data_frame_to_json(pandemic_recovery_df)[6]
     assert inglewood_pizza["name"] == "Inglewood Pizza"
     assert inglewood_pizza["num_reviews"] == 1
-    pandemic_recovery_df = pandemic_recovery_df.withColumn("num_interactions",
-                                                           pandemic_recovery_df.num_reviews +
-                                                           pandemic_recovery_df.num_tips +
-                                                           pandemic_recovery_df.num_checkins)
+
 
 
 
