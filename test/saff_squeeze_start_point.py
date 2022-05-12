a6003b64bc9a6f5f7a46c9977f78ee9de3e7d3ef
jmasonlee <jmasonlee@gmail.com>
2022-05-12 16:40:40 -0600

    t  duplicate test

--- test/saff_squeeze_start_point.py
+++ test/saff_squeeze_start_point.py
@@ -27,6 +27,28 @@ def test_will_do_the_right_thing(spark: SparkSession) -> None:
     #save_results_to_expected(actual_df)
 
 
+def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
+    b_reviews_df = create_df_from_json("fixtures/browser_reviews.json", spark)
+    checkin_df   = create_df_from_json("fixtures/checkin.json", spark)
+    tips_df      = create_df_from_json("fixtures/tips.json", spark)
+    business_df  = create_df_from_json("fixtures/business.json", spark)
+    m_reviews_df = create_df_from_json("fixtures/mobile_reviews.json", spark)
+
+    actual_df = transform(
+        business_df,
+        checkin_df,
+        b_reviews_df,
+        tips_df,
+        m_reviews_df,
+        datetime(2022, 4, 14)
+    )
+
+    expected_json = read_json()
+    assert data_frame_to_json(actual_df) == expected_json
+    #save_results_to_expected(actual_df)
+
+
+
 def save_results_to_expected(actual_df):
     with open("fixtures/expected.json", "w") as f:
         jsons = ''.join(
