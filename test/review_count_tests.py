429e9b316098b552f46de534dd248e49fdafba33
jmasonlee <jmasonlee@gmail.com>
2022-05-12 17:39:50 -0600

    wait to declare the variable until it's actually used

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -8,10 +8,10 @@ from test.saff_squeeze_start_point import create_df_from_json, data_frame_to_jso
 ############################# SAFF SQUEEZE #################################
 def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
     b_reviews_df = create_df_from_json("fixtures/browser_reviews.json", spark)
-    checkin_df   = create_df_from_json("fixtures/checkin.json", spark)
     m_reviews_df = create_df_from_json("fixtures/mobile_reviews.json", spark)
-
     date = datetime(2022, 4, 14)
+
+    checkin_df = create_df_from_json("fixtures/checkin.json", spark)
     checkin_df = create_checkin_df_with_one_date_per_row(checkin_df)
     reviews_df = count_reviews(checkin_df, m_reviews_df, b_reviews_df, date)
 
