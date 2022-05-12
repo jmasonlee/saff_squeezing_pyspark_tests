64f5a2de0508fb39ec3cc76b40197090934b8481
jmasonlee <jmasonlee@gmail.com>
2022-05-12 17:04:10 -0600

    t inline

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -2,8 +2,9 @@ from datetime import datetime
 
 from pyspark.sql import SparkSession
 
-from pandemic_recovery_batch import transform
-from test.saff_squeeze_start_point import create_df_from_json, data_frame_to_json, read_json
+from pandemic_recovery_batch import construct_post_pandemic_recovery_df, \
+    create_checkin_df_with_one_date_per_row, count_reviews, count_checkins, count_tips
+from test.saff_squeeze_start_point import create_df_from_json, data_frame_to_json
 
 
 def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
@@ -13,15 +14,16 @@ def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
     business_df  = create_df_from_json("fixtures/business.json", spark)
     m_reviews_df = create_df_from_json("fixtures/mobile_reviews.json", spark)
 
-    actual_df = transform(
-        business_df,
-        checkin_df,
-        b_reviews_df,
-        tips_df,
-        m_reviews_df,
-        datetime(2022, 4, 14)
+    date = datetime(2022, 4, 14)
+    checkin_df = create_checkin_df_with_one_date_per_row(checkin_df)
+    reviews_df = count_reviews(checkin_df, m_reviews_df, b_reviews_df, date)
+    checkin_df = count_checkins(checkin_df, date)
+    tips_df = count_tips(tips_df, date)
+    actual_df = construct_post_pandemic_recovery_df(
+        business_df, checkin_df, reviews_df, date, tips_df
     )
 
     inglewood_pizza = data_frame_to_json(actual_df)[6]
     assert inglewood_pizza["name"] == "Inglewood Pizza"
     assert inglewood_pizza["num_reviews"] == 1
+
