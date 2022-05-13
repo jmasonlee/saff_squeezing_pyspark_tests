3b096f0dd5bfaa47a38ec21f4cdc7d70b9acd956
jmasonlee <jmasonlee@gmail.com>
2022-05-12 18:02:29 -0600

    Use a duplicated browser_reviews.json

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -8,7 +8,7 @@ from test.saff_squeeze_start_point import create_df_from_json, data_frame_to_jso
 
 ############################# SAFF SQUEEZE #################################
 def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
-    b_reviews_df = create_df_from_json("fixtures/browser_reviews.json", spark)
+    b_reviews_df = create_df_from_json("fixtures/browser_reviews2.json", spark)
     m_reviews_df = create_df_from_json("fixtures/mobile_reviews.json", spark)
     date = datetime(2022, 4, 14)
 
