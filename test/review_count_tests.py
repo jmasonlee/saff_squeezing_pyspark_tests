5aa5f40d397c8eb2b69c0462eed75ce864c68e3b
jmasonlee <jmasonlee@gmail.com>
2022-05-12 18:08:36 -0600

    Use new json for browser reviews

--- test/review_count_tests.py
+++ test/review_count_tests.py
@@ -16,7 +16,7 @@ def test_keeps_mobile_reviews_without_checkins(spark: SparkSession) -> None:
     reviews_df = count_reviews(checkin_df, m_reviews_df, b_reviews_df, date)  # <- This is what we care about
 
 
-    inglewood_pizza = data_frame_to_json(reviews_df)[6]
+    inglewood_pizza = data_frame_to_json(reviews_df.where( reviews_df.business_id == "mpf3x-BjTdTEA3yCZrAYPw"))[0]
     assert inglewood_pizza["business_id"] == "mpf3x-BjTdTEA3yCZrAYPw"
     assert inglewood_pizza["num_reviews"] == 1
 ############################# SAFF SQUEEZE #################################
