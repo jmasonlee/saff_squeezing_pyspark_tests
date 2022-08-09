a20f7b8311cf2e0f7e6466cdd3b4a316a3be7d0e
jmasonlee <jmasonlee@gmail.com>
2022-08-09 18:53:21 +0000

    Rt Remove approvaltests
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>
    Co-Authored-By: Balazs Reho <reho.balazs@gmail.com>

--- test/applesauce_test.py
+++ test/applesauce_test.py
@@ -1,20 +1,14 @@
 from datetime import datetime
 
-import approvaltests
+
 from chispa import assert_df_equality
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
+from pyspark.sql.types import StructType, StructField, StringType, LongType
 
 from pandemic_recovery_batch import count_interactions_from_reviews
 
 
 def create_df(spark, schema, data):
     return spark.createDataFrame(schema=schema, data=data)
-
-def verify_df(df):
-    # use count() to ensure we display every row
-    # truncate as 0 means do not truncate at all
-    # false to not show data vertically
-    approvaltests.verify(df._jdf.showString(df.count(), 0, False))
 
 
 SCHEMA = StructType([
@@ -34,7 +28,6 @@ def test_keeps_mobile_reviews_without_checkins(spark):
 
     expected_df = create_df(spark, SCHEMA2, [{ 'business_id': 'bid', 'num_reviews': 1}])
     assert_df_equality(reviews_df, expected_df)
-    # verify_df(reviews_df
 
 def test_does_not_count_mobile_reviews_with_checkins(spark):
     mobile_df = create_df(spark, SCHEMA, [{ 'business_id': 'bid', 'user_id': 'uid', 'date': '2022-04-14'}])
