c91b44519e05179fb40dfd597c908ea7fed48b99
jmasonlee <jmasonlee@gmail.com>
2022-08-23 18:40:08 +0000

    delete decorator option
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>
    Co-Authored-By: Balazs Reho <reho.balazs@gmail.com>

--- test/decorators.py
+++ test/decorators.py
@@ -53,30 +53,3 @@ def test_composition(spark):
     __ = TestDataFrame(spark).with_schema_from(mobile_review_df).create_df()
     reviews_df = count_interactions_from_reviews(__, mobile_review_df, __, datetime(2022, 4, 14))
     assert reviews_df.count() == 1
-
-
-#
-def add_empty_dfs(func, *args, **kwargs):
-    def inner(applesauce):
-        @functools.wraps(func)
-        def wrapper_do_twice(*args, **kwargs):
-            func(*args, **kwargs)
-            return func(*args, **kwargs)
-        return wrapper_do_twice
-    return inner
-
-@pytest.mark.skip()
-def test_decorator(spark):
-    @add_empty_dfs(applesauce='important_df')
-    def function_under_test(important_df, empty_df):
-        assert important_df.columns == empty_df.columns
-        assert empty_df.count() == 0
-
-    important_df = spark.createDataFrame(data=[{'key': 'value'}])
-    empty_df = spark.createDataFrame(data=[{}])
-    function_under_test(important_df=important_df, empty_df=empty_df)
-
-#Balazs idea
-
-#with empty_dataframe_form(important_df) as empy:
-#    transform(...)
