c61242fb1a11e85f2e754484ce6dfdf8062c8853
jmasonlee <jmasonlee@gmail.com>
2022-08-23 18:39:01 +0000

    Built test dataframe
    Co-Authored-By: brianberzins <2465816+brianberzins@users.noreply.github.com>

--- test/decorators.py
+++ test/decorators.py
@@ -1,5 +1,60 @@
 import functools
 
+# Balazs idea
+from datetime import datetime
+
+import pytest
+
+from pandemic_recovery_batch import count_interactions_from_reviews
+
+
+class TestDataFrame:
+    def __init__(self, spark):
+        self.spark = spark
+        self.schema = []
+
+    def __enter__(self):
+        return self.create_df()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def with_schema_from(self, reference_df):
+        self.schema = reference_df.schema
+        return self
+
+    def create_df(self):
+        return self.spark.createDataFrame(schema=self.schema, data=[{}])
+
+
+class EmptyDataFrame:
+    def __init__(self, spark):
+        self.spark = spark
+        self.schema = []
+
+    def __enter__(self):
+        return self.spark.createDataFrame(schema=self.schema, data=[{}])
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def with_schema_from(self, reference_df):
+        self.schema = reference_df.schema
+        return self
+
+def test_context(spark):
+    mobile_review_df = spark.createDataFrame(data=[{'business_id': 'bid', 'user_id': 'uid', 'date': '2022-04-14'}])
+    with TestDataFrame(spark).with_schema_from(mobile_review_df) as __:
+        reviews_df = count_interactions_from_reviews(__, mobile_review_df, __, datetime(2022, 4, 14))
+        assert reviews_df.count() == 1
+
+def test_composition(spark):
+    mobile_review_df = spark.createDataFrame(data=[{'business_id': 'bid', 'user_id': 'uid', 'date': '2022-04-14'}])
+    __ = TestDataFrame(spark).with_schema_from(mobile_review_df).create_df()
+    reviews_df = count_interactions_from_reviews(__, mobile_review_df, __, datetime(2022, 4, 14))
+    assert reviews_df.count() == 1
+
+
 #
 def add_empty_dfs(func, *args, **kwargs):
     def inner(applesauce):
@@ -10,6 +65,7 @@ def add_empty_dfs(func, *args, **kwargs):
         return wrapper_do_twice
     return inner
 
+@pytest.mark.skip()
 def test_decorator(spark):
     @add_empty_dfs(applesauce='important_df')
     def function_under_test(important_df, empty_df):
