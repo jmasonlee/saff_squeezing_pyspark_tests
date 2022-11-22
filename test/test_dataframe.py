549c68dc91fd3ec1392f85210a971d6f35434109
jmasonlee <jmasonlee@gmail.com>
2022-11-22 19:56:42 +0000

    R  Extract variables and method

--- test/test_dataframe.py
+++ test/test_dataframe.py
@@ -75,13 +75,16 @@ class TestDataFrame:
         | 2000-01-01 04:05:06 | 3     |
         """
         column_names = self.get_column_names(table)
-        stars = [5, 3]
-        dates = ["2000-01-02 03:04:05", "2000-01-01 04:05:06"]
+        stars = self.get_column_data(table, 0)
+        dates = self.get_column_data(table, 1)
         self.data = [
             {column_names[0]: stars[0], column_names[1]: dates[0]},
             {column_names[0]: stars[1], column_names[1]: dates[1]}
         ]
         return self
+
+    def get_column_data(self, table, column_index):
+        return [5, 3] if column_index == 0 else ["2000-01-02 03:04:05", "2000-01-01 04:05:06"]
 
     def get_column_names(self, table):
         return ["stars", "date"]
