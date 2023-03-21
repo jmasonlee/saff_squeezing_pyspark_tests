402f1f72f3beb51e21a5a3c0b715a2a72ce8e77b
jmasonlee <jmasonlee@gmail.com>
2023-03-21 13:28:07 -0600

    R##  Extract business id

--- test/pandemic_recovery_batch_test.py
+++ test/pandemic_recovery_batch_test.py
@@ -10,20 +10,21 @@ from pandemic_recovery_batch import count_checkins
 def test_will_only_count_checkins_from_rundate(spark: SparkContext) -> None:
     run_date = datetime.datetime(2001, 1, 1)
     one_day = datetime.timedelta(days=1)
+    business_id = "business_1"
     checkins_df = TestDataFrame(spark).create_test_dataframe_from_table(
         f"""
         date                    | business_id
-        {run_date}              | 1
-        {run_date}              | 1
-        {run_date - one_day}    | 1
+        {run_date}              | {business_id}
+        {run_date}              | {business_id}
+        {run_date - one_day}    | {business_id}
         """
     ).create_spark_df()
 
     actual_df = count_checkins(checkins_df, run_date)
     expected_df = TestDataFrame(spark).create_test_dataframe_from_table(
-        """
-        business_id | num_checkins
-        1           | 2
+        f"""
+        business_id     | num_checkins
+        {business_id}   | 2
         """
     ).create_spark_df()
     assert_df_equality(expected_df, actual_df, ignore_nullable=True)
