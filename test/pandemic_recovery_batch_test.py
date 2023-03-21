6e80a223c3dcff080d99a64e68d61827eb55c175
jmasonlee <jmasonlee@gmail.com>
2023-03-21 13:35:42 -0600

    R  Simplify expected_df creation

--- test/pandemic_recovery_batch_test.py
+++ test/pandemic_recovery_batch_test.py
@@ -11,20 +11,15 @@ def test_will_only_count_checkins_from_rundate(spark: SparkContext) -> None:
     run_date = datetime.datetime(2001, 1, 1)
     one_day = datetime.timedelta(days=1)
     business_id = "business_1"
-    checkins_df = TestDataFrame(spark).create_test_dataframe_from_table(
+    checkins_df = TestDataFrame(spark).with_base_data(business_id=business_id).create_test_dataframe_from_table(
         f"""
-        date                    | business_id
-        {run_date}              | {business_id}
-        {run_date}              | {business_id}
-        {run_date - one_day}    | {business_id}
+        date 
+        {run_date}            
+        {run_date}            
+        {run_date - one_day}  
         """
     ).create_spark_df()
 
     actual_df = count_checkins(checkins_df, run_date)
-    expected_df = TestDataFrame(spark).create_test_dataframe_from_table(
-        f"""
-        business_id     | num_checkins
-        {business_id}   | 2
-        """
-    ).create_spark_df()
+    expected_df = TestDataFrame(spark).with_base_data(business_id=business_id).with_data([{"num_checkins": 2}]).create_spark_df()
     assert_df_equality(expected_df, actual_df, ignore_nullable=True)
